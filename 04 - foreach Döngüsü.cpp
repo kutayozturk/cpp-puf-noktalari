1940e4908a0f512f40009c51c2a3d209f83048de
Kutay ÖZTÜRK <94574681+kutayozturk@users.noreply.github.com>
2023-02-16 05:41:24 +0300

    Add files via upload

---
+++ 04 - foreach Döngüsü.cpp
@@ -0,0 +1,41 @@
+//	C++ Foreach Döngüsü
+
+int a[] = {10,20,30,40,50}
+for (int i=0; i<5; i++){
+	cout << a[i] << endl;
+}
+
+/*
+Yukarıda standart bir for döngüsü kullanılmıştır. 
+
+Ekran Çıktısı
+10
+20
+30
+40
+50
+
+bu döngü ile a dizsinin tüm elemanlarını ekrana yazdırmış olduk.
+
+foreach döngüsü, bir dizinin tüm elemanlarına daha kısa bir şekilde ulaşmamızı sağlar.
+foreach döngüsü diziler ile birlikte kullanılır.
+Yukarıdaki örneği foreach ile tekrar yapalım
+*/
+
+int a[] = {10,20,30,40,50}
+for(int item:a){
+	cout << item <<endl;
+}
+
+/*
+item isminde bir değişken tanımladık. Bu değişken "a" dizinin her bir elemanını temsi eder.
+daha sonras cout komutu ile item değişkeninin ekrana yazdırdık.
+
+Ekran Çıktısı
+10
+20
+30
+40
+50
+
+*/
